0efaafb54c4feab4e1ed21b018a8cd60c2536edb
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-12 17:48:00 +0200

    indexOF

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@ var arrCognomi = ["Ubaldo", "De Rossi", "Rosa"];
 
 var cognome = prompt("Inserisci cognome.");
 arrCognomi.push(cognome);
+
 arrCognomi.sort();
+
 for(var i = 0; i < arrCognomi.length; i++){
   elementoCognome = arrCognomi[i];
   
+  //procedura per far stampare l'arrai sottoforma di lista nell'html
   var elementoCognomeHTML = document.getElementById("lista").innerHTML;
   document.getElementById("lista").innerHTML = elementoCognomeHTML + "<li>" + elementoCognome + "</li>";  
 }
+
+//posizionamento nome utente
+var posizioneCognome = cognome.indexOf(arrCognomi);
+
+document.getElementById("posizione").innerHTML = cognome + " é " + (posizioneCognome + 1);
 
